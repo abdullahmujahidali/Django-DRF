d7c126b554d30c9909fd5448de3cb7f848b5bdfc
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-04-29 12:21:11 +0500

    fixing minor issues in app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url("rest-auth/", include("rest_auth.urls")),
+    path("rest-auth/", include("rest_auth.urls")),
     path('api-auth/', include('rest_framework.urls')),
-    path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
