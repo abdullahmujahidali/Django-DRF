d059186292f3dcb76fdc4a3cf606759b6f76f2f8
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-04-29 12:39:40 +0500

    configured restframework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,6 +100,12 @@ AUTHENTICATION_BACKENDS = [
 
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
