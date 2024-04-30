bc92843376442d4b23514d4b08ebb21483d8e62a
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-04-30 13:10:09 +0500

    install and configured simple_jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,7 +104,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
