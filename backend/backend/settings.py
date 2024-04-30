59d6ff063c27acf7c138417353685f06b6c65cf0
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-04-30 13:12:20 +0500

    added throttling

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,7 +110,15 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 # Password validation
